877b92a85bb1eaa5aa3bc64e16376a73bebc3909
Salim Lou <saaliim8@gmail.com>
2020-04-29 19:11:29 +0100

    HomeInterface DrawerNavigation Added ActionBar Added BottomActionBar Added

--- app/src/main/java/com/example/akhbariapp/MainActivity.java
+++ app/src/main/java/com/example/akhbariapp/MainActivity.java
@@ -1,20 +1,35 @@
 package com.example.akhbariapp;
 
 import androidx.annotation.ContentView;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.home_interface);
 
-        TextView signUp = findViewById(R.id.signUp);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        /*TextView signUp = findViewById(R.id.signUp);
         TextView logIn  = findViewById(R.id.logIn);
 
             signUp.setOnClickListener(new View.OnClickListener() {
@@ -22,6 +37,14 @@ public class MainActivity extends AppCompatActivity {
                 public void onClick(View v) {
                     setContentView(R.layout.sign_up_fragment);
                 }
-            });
+            });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_menu,menu);
+        return true;
     }
 }
