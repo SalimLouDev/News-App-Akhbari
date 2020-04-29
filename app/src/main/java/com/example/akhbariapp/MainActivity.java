de74412cf06dfb66e5a90ae6aeeb2d51290284e9
Salim Lou <saaliim8@gmail.com>
2020-04-29 13:47:33 +0100

    SignUpInterfaceV1

--- app/src/main/java/com/example/akhbariapp/MainActivity.java
+++ app/src/main/java/com/example/akhbariapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.akhbariapp;
 
+import androidx.annotation.ContentView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        TextView signUp = findViewById(R.id.signUp);
+        TextView logIn  = findViewById(R.id.logIn);
+
+            signUp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setContentView(R.layout.sign_up_fragment);
+                }
+            });
     }
 }
