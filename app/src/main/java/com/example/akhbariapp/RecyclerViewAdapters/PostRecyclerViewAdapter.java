e9122e738d91dd7ca79414d6e27890e3ad0a9f7a
oucifmohamed <43790630+oucifmohamed@users.noreply.github.com>
2020-05-08 19:19:15 +0100

    -The onclick in the recyclerviewadapter

--- app/src/main/java/com/example/akhbariapp/RecyclerViewAdapters/PostRecyclerViewAdapter.java
+++ app/src/main/java/com/example/akhbariapp/RecyclerViewAdapters/PostRecyclerViewAdapter.java
@@ -5,8 +5,6 @@ import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Filter;
-import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,36 +18,35 @@ import com.example.akhbariapp.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerViewAdapter.ViewHolder> implements Filterable {
+public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerViewAdapter.ViewHolder>{
 
     private Context context;
     private LayoutInflater layoutInflater;
-    private List<PostsEntity>full_posts;
-    private List<PostsEntity>full_posts_copy = new ArrayList<>();
-
-    public PostRecyclerViewAdapter(Context context) {
+    private List<PostsEntity>posts = new ArrayList<>();
+    private OnpostClickListner listner;
+    public PostRecyclerViewAdapter(Context context,OnpostClickListner listner) {
         this.context = context;
+        this.listner = listner;
         layoutInflater = LayoutInflater.from(context);
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
         View root = layoutInflater.inflate(R.layout.post_item,parent,false);
-        return new ViewHolder(root);
+        return new ViewHolder(root,listner);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
-         holder.post_title.setText(full_posts_copy.get(position).getTitle());
+         holder.post_title.setText(posts.get(position).getTitle());
          holder.read_more.setText(R.string.read_more);
-         holder.post_date.setText(full_posts_copy.get(position).getPost_date().toString("dd-MM-yyyy"));
-         holder.post_time.setText(full_posts_copy.get(position).getPost_time().toString("HH:mm"));
+         holder.post_date.setText(posts.get(position).getPost_date().toString("dd-MM-yyyy"));
+         holder.post_time.setText(posts.get(position).getPost_time().toString("HH:mm"));
 
          ImageView post_image = holder.post_image;
-         Uri image_uri = Uri.parse(full_posts_copy.get(position).getImage_uri());
+         Uri image_uri = Uri.parse(posts.get(position).getImage_uri());
 
          Glide.with(context)
                 .load(image_uri)
@@ -59,61 +56,65 @@ public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerVi
 
     @Override
     public int getItemCount() {
-        return full_posts_copy.size();
+        return posts.size();
     }
 
-    public void setList(List<PostsEntity>full_posts_copy){
-        this.full_posts_copy = full_posts_copy;
-        this.full_posts = new ArrayList<>(this.full_posts_copy);
+    public void setList(List<PostsEntity>posts){
+        this.posts = posts;
         notifyDataSetChanged();
     }
 
-    @Override
-    public Filter getFilter() {
-        return search_post_filter;
-    }
+//    @Override
+//    public Filter getFilter() {
+//        return search_post_filter;
+//    }
+//
+//     private Filter search_post_filter = new Filter() {
+//         @Override
+//         protected FilterResults performFiltering(CharSequence constraint) {
+//             List<PostsEntity> filtered_list = new ArrayList<>();
+//
+//             if(constraint.toString().isEmpty()){
+//                 filtered_list.addAll(posts);
+//
+//             }else {
+//                 String filter_pattern = constraint.toString().toLowerCase().trim();
+//                 filtered_list.addAll((Collection<? extends PostsEntity>) postsViewModel.get_posts_by_name(filter_pattern));
+//             }
+//
+//             FilterResults filterResults = new FilterResults();
+//             filterResults.values = filtered_list;
+//             return filterResults;
+//         }
+//
+//         @Override
+//         protected void publishResults(CharSequence constraint, FilterResults results) {
+//            posts= (List) results.values;
+//            notifyDataSetChanged();
+//         }
+//     };
 
-     private Filter search_post_filter = new Filter() {
-         @Override
-         protected FilterResults performFiltering(CharSequence constraint) {
-             List<PostsEntity> filtered_list = new ArrayList<>();
-
-             if(constraint==null || constraint.length()==0){
-                 filtered_list.addAll(full_posts);
-
-             }else {
-                 String filter_pattern = constraint.toString().toLowerCase().trim();
-                 for(PostsEntity postsEntity : full_posts){
-                     if(postsEntity.getTitle().toLowerCase().contains(filter_pattern)){
-                         filtered_list.add(postsEntity);
-                     }
-                 }
-             }
-
-             FilterResults filterResults = new FilterResults();
-             filterResults.values = filtered_list;
-             return filterResults;
-         }
-
-         @Override
-         protected void publishResults(CharSequence constraint, FilterResults results) {
-            full_posts_copy.clear();
-            full_posts_copy.addAll((List) results.values);
-            notifyDataSetChanged();
-         }
-     };
-
-    static class ViewHolder extends RecyclerView.ViewHolder{
+     class ViewHolder extends RecyclerView.ViewHolder{
 
         private ImageView post_image;
         private TextView post_title,read_more,post_date,post_time;
-        ViewHolder(@NonNull View itemView) {
+        private PostRecyclerViewAdapter.OnpostClickListner listner_2;
+        ViewHolder(@NonNull View itemView,PostRecyclerViewAdapter.OnpostClickListner listner_2) {
             super(itemView);
             post_image = itemView.findViewById(R.id.post_image);
             post_title = itemView.findViewById(R.id.post_title);
             read_more = itemView.findViewById(R.id.post_read_more);
             post_date = itemView.findViewById(R.id.post_date);
             post_time = itemView.findViewById(R.id.post_time);
+            this.listner_2 = listner_2;
+
+            //itemView.setOnClickListener(v -> listner.onclick(posts.get(getAdapterPosition())));
+
+            read_more.setOnClickListener(v-> listner_2.onclick(posts.get(getAdapterPosition())));
         }
+    }
+
+    public interface OnpostClickListner{
+        void onclick(PostsEntity postsEntity);
     }
 }
