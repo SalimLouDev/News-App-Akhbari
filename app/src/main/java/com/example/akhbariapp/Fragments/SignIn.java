90ca621556032f7278d61f5b9487fa085b09c901
Salim Lou <saaliim8@gmail.com>
2020-05-09 18:31:15 +0100

    bug fixes

--- app/src/main/java/com/example/akhbariapp/Fragments/SignIn.java
+++ app/src/main/java/com/example/akhbariapp/Fragments/SignIn.java
@@ -1,3 +1,4 @@
+
 package com.example.akhbariapp.Fragments;
 
 import android.content.Intent;
@@ -7,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,51 +30,55 @@ import java.util.concurrent.ExecutionException;
 
 import static android.content.Context.MODE_PRIVATE;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class SignIn extends Fragment {
 
-    private TextInputLayout national_id,password;
+    private TextInputLayout national_id, password;
     private UserViewModel userViewModel;
-
+    private CheckBox remember_me_checkbox;
+    private SharedPreferences admin;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.login,container,false);
-        SharedPreferences admin = Objects.requireNonNull(getActivity()).getSharedPreferences("Admin", MODE_PRIVATE);
+        View root = inflater.inflate(R.layout.login, container, false);
+        admin = Objects.requireNonNull(getActivity()).getSharedPreferences("Admin", MODE_PRIVATE);
         national_id = root.findViewById(R.id.nat_edit_text_sign_in);
-        if(!admin.getString("access","yes").equals("no")){
+        remember_me_checkbox = root.findViewById(R.id.remember_checkbox);
+        if (!admin.getString("access", "yes").equals("no")) {
 
-        if(!admin.getString("first_name","no").equals("no") && admin.getString("user_type","no").equals("admin")){
-            Intent intent = new Intent(getActivity(), AdminHomeActivity.class);
-            intent.putExtra("user","admin");
-            intent.putExtra("id", Objects.requireNonNull(national_id.getEditText()).getText().toString());
-            startActivity(intent);
-            Objects.requireNonNull(getActivity()).finish();
+            if (!admin.getString("first_name", "no").equals("no") && admin.getString("user_type", "no").equals("admin")) {
+                Intent intent = new Intent(getActivity(), AdminHomeActivity.class);
+                intent.putExtra("user", "admin");
+                intent.putExtra("id", Objects.requireNonNull(national_id.getEditText()).getText().toString());
+                startActivity(intent);
+                Objects.requireNonNull(getActivity()).finish();
 
-        }else if (!admin.getString("first_name","no").equals("no") && admin.getString("user_type","no").equals("normal_user")){
-            Intent intent = new Intent(getActivity(), HomeActivity.class);
-            intent.putExtra("user","normal_user");
-            intent.putExtra("national_id", Objects.requireNonNull(national_id.getEditText()).getText().toString());
-            startActivity(intent);
-            Objects.requireNonNull(getActivity()).finish();
-         }
+            } else if (!admin.getString("first_name", "no").equals("no") && admin.getString("user_type", "no").equals("normal_user")) {
+                Intent intent = new Intent(getActivity(), HomeActivity.class);
+                intent.putExtra("user", "normal_user");
+                intent.putExtra("national_id", Objects.requireNonNull(national_id.getEditText()).getText().toString());
+                startActivity(intent);
+                Objects.requireNonNull(getActivity()).finish();
+            }
         }
 
         Button sign_in = root.findViewById(R.id.login_button);
         TextView sign_up_text = root.findViewById(R.id.signUp);
 
-        sign_in.setOnClickListener(v->{
+        sign_in.setOnClickListener(v -> {
             try {
                 login();
             } catch (ExecutionException | InterruptedException e) {
                 e.printStackTrace();
             }
         });
-        sign_up_text.setOnClickListener(v->{
+        sign_up_text.setOnClickListener(v -> {
             FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = Objects.requireNonNull(fragmentManager).beginTransaction();
 
             SignUp signUpFragment = new SignUp();
-            fragmentTransaction.replace(R.id.fragment_container_sign,signUpFragment,"tag");
+            fragmentTransaction.replace(R.id.fragment_container_sign, signUpFragment, "tag");
             fragmentTransaction.commit();
         });
 
@@ -83,36 +89,58 @@ public class SignIn extends Fragment {
 
         return root;
     }
-
     private void login() throws ExecutionException, InterruptedException {
 
-       if ((Objects.requireNonNull(national_id.getEditText()).getText().toString().trim().isEmpty() ||
-           Objects.requireNonNull(password.getEditText()).getText().toString().trim().isEmpty())){
-           Toast.makeText(getContext(),"You have to fill both of the fields",Toast.LENGTH_SHORT).show();
-       }else {
-           String _password = Objects.requireNonNull(password.getEditText()).getText().toString().trim();
-           String nat_id = national_id.getEditText().getText().toString().trim();
+        SharedPreferences.Editor fields_save = admin.edit();
+        if ((Objects.requireNonNull(national_id.getEditText()).getText().toString().trim().isEmpty() ||
+                Objects.requireNonNull(password.getEditText()).getText().toString().trim().isEmpty())){
+            Toast.makeText(getContext(),"You have to fill both of the fields",Toast.LENGTH_SHORT).show();
+        }else {
+            String _password = Objects.requireNonNull(password.getEditText()).getText().toString().trim();
+            String nat_id = national_id.getEditText().getText().toString().trim();
 
-           EntityUser user = userViewModel.find(_password,nat_id);
+            EntityUser user = userViewModel.find(_password,nat_id);
 
-           if (user==null){
-               Toast.makeText(getContext(),"Your password or national_id is false",Toast.LENGTH_SHORT).show();
-           }else {
-               if(user.getAdmin_code()==1234){
-                       Intent intent = new Intent(getActivity(), AdminHomeActivity.class);
-                       intent.putExtra("user","admin");
-                       intent.putExtra("id", Objects.requireNonNull(national_id.getEditText()).getText().toString());
-                       startActivity(intent);
-                       Objects.requireNonNull(getActivity()).finish();
+            if (user==null){
+                Toast.makeText(getContext(),"Your password or national_id is false",Toast.LENGTH_SHORT).show();
+            }else {
+                if(user.getAdmin_code()==1234){
+                    if(remember_me_checkbox.isChecked()){
+                        fields_save.putString("nat_id",nat_id);
+                        fields_save.putString("password",_password);
+                        fields_save.putBoolean("remember",true);
+                        fields_save.apply();
+                    }else {
+                        fields_save.putString("nat_id",nat_id);
+                        fields_save.putString("password",_password);
+                        fields_save.putBoolean("remember",false);
+                        fields_save.apply();
+                    }
+                    Intent intent = new Intent(getActivity(), AdminHomeActivity.class);
+                    intent.putExtra("user","admin");
+                    intent.putExtra("id", Objects.requireNonNull(national_id.getEditText()).getText().toString());
+                    startActivity(intent);
+                    Objects.requireNonNull(getActivity()).finish();
 
-                   }else if(user.getAdmin_code()==0){
-                       Intent intent = new Intent(getActivity(), HomeActivity.class);
-                       intent.putExtra("user","normal_user");
-                      intent.putExtra("id", Objects.requireNonNull(national_id.getEditText()).getText().toString());
-                       startActivity(intent);
-                       Objects.requireNonNull(getActivity()).finish();
-                   }
-               }
-           }
-       }
+                }else if(user.getAdmin_code()==0){
+                    if(remember_me_checkbox.isChecked()){
+                        fields_save.putString("nat_id",nat_id);
+                        fields_save.putString("password",_password);
+                        fields_save.putBoolean("remember",true);
+                        fields_save.apply();
+                    }else {
+                        fields_save.putString("nat_id",nat_id);
+                        fields_save.putString("password",_password);
+                        fields_save.putBoolean("remember",false);
+                        fields_save.apply();
+                    }
+                    Intent intent = new Intent(getActivity(), HomeActivity.class);
+                    intent.putExtra("user","normal_user");
+                    intent.putExtra("id", Objects.requireNonNull(national_id.getEditText()).getText().toString());
+                    startActivity(intent);
+                    Objects.requireNonNull(getActivity()).finish();
+                }
+            }
+        }
     }
+}
